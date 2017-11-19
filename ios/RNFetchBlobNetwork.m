356eeab46487ab7f503596103ef94eb9c8e01e05
jiabinxu <284696433@qq.com>
2017-11-19 10:36:48 +0800

    Fix iOS initialization race condition #499

--- ios/RNFetchBlobNetwork.m
+++ ios/RNFetchBlobNetwork.m
@@ -106,8 +106,14 @@ NSOperationQueue *taskQueue;
 - (id)init {
     self = [super init];
     if(taskQueue == nil) {
-        taskQueue = [[NSOperationQueue alloc] init];
-        taskQueue.maxConcurrentOperationCount = 10;
+        // taskQueue = [[NSOperationQueue alloc] init];
+        // taskQueue.maxConcurrentOperationCount = 10;
+        @synchronized ([RNFetchBlobNetwork class]) {
+            if (taskQueue == nil) {
+                taskQueue = [[NSOperationQueue alloc] init];
+                taskQueue.maxConcurrentOperationCount = 10;
+            }
+        }
     }
     return self;
 }
